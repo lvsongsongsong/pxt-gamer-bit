d78dfca1046097fc953da5cb97813c7dda68689d
lvsongsongsong <33586095+lvsongsongsong@users.noreply.github.com>
2018-11-08 15:07:33 +0800

    Update and rename gamerbit.cpp to gamebit.cpp

--- gamebit.cpp
+++ gamebit.cpp
@@ -1,7 +1,7 @@
 #include "pxt.h"
 
 using namespace pxt;
-namespace gamerbit {
+namespace gamebit {
     bool initialized = false;
 
     //%
@@ -11,12 +11,10 @@ namespace gamerbit {
     // mount buttons on the pins with a pullup mode
     // TODO: fix this issue in the DAL itself
 #define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P0)
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P1)
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P2)
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P8)
     ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P12)
-    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
+    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P13)
+    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P14)
+    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P15)
 #undef ALLOC_PIN_BUTTON
 
         initialized = true;
